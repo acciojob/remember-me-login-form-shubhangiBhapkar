5ab06ec37859b794d287dd363c458bdd83d8cd5f
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-08-14 08:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const userName = document.getElementById("id");
+const password = document.getElementById("password");
+const checkBox = document.getElementById("checkbox");
+const btn = document.getElementById("submit");
+const myform = document.getElementById("myForm");
+const existingbtn = document.getElementById("existing");
+
+myForm.addEventListener("submit", function(e){
+	e.preventDefault();
+
+	if(localStorage.userObj.userName == userName.value && 
+	   localStorage.userObj.password == password.value
+	  ){
+		existingbtn.style.display = block;
+	  }
+
+	alert(`Logged in as ${userName.value}`);
+	let userObj = {username:userName, password:password}
+	if(checkBox.value == checked){
+		localStorage.setItem(JSON.stringify(userObj));
+	}else{
+		localStorage.clear();
+	}
+
+	
+})
+
+existingbtn.addEventListener("click",function(){
+	alert("Logged in as <saved username>")
+})
